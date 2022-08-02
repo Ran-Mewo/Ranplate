9424a223c0f7354f6123cb2ac5a42d795d5b676f
Ran <43445785+RanCraftPlayz@users.noreply.github.com>
2022-08-02 19:41:43 +0600

    pls be final error of 1.16.5

--- forge/src/main/java/net/examplemod/forge/ExampleModForge.java
+++ forge/src/main/java/net/examplemod/forge/ExampleModForge.java
@@ -1,7 +1,11 @@
 package net.examplemod.forge;
 
 import com.llamalad7.mixinextras.MixinExtrasBootstrap;
+#if POST_MC_1_16_5
 import dev.architectury.platform.forge.EventBuses;
+#else
+import me.shedaniel.architectury.platform.forge.EventBuses;
+#endif
 import net.examplemod.ExampleMod;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
